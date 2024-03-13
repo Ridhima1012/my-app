8327c6423510fb67251b128092e03d5fafaa0493
Ridhima1012 <ridhima1012c@gmail.com>
2024-03-14 01:31:30 +0530

    HELLO R

---
+++ src/app.test.js
@@ -0,0 +1,11 @@
+// app.test.js
+
+const { sum, subtract } = require('./app');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 2 from 5 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
